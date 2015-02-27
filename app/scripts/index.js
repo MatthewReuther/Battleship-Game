6f315e98b9914b9ae2b6f745e25800f068339268
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-27 16:13:30 -0600

    added some jquery to try to start appending a game board to the page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,13 +1,39 @@
-'use strict';
+
 
 //Declare Variables
-
-
+  var fb = new Firebase (),
+      createGame,
+      array       = ['', '', '', '', '', '', '', '', '', ''],
+      board       = [array, array, array, array, array, array, array, array, array, array],
+      player1     = 'Soviet Union',
+      player2     = 'United States',
+      playerTurn,
+      index;
 
 
 //Begin Functions
   //createGame
+    $('#newGame').on('click', function(event){
+      event.preventDefault();
+      createBoard(board);
+
+
+    });
   //createBoard ===== think about using using .oneClick() or .unbind()
+    function createBoard(board) {
+    var $table = $('<table class="gameBoard" ></table>')              //Created table to append too
+    board.forEach(function (createRow) {                              //For Each item in the board array and create a row for it
+      var $tr = $('<tr></tr>');                                       //created table rows in table
+      createRow.forEach(function (createTd){                          //For Each table row create a TD
+        $tr.append('<td class="createTd">' + createTd + '</td>')      //Nested loop going through every '' within the array
+      });
+    $table.append($tr);                                               //Append the created Td's to the table
+    });
+    $('board').append($table);                                        //Append the tbale to the the actual game board
+  }
+
+
+
     //createGame
   //addPlayer
 
@@ -20,5 +46,4 @@
 
 
 
-}
 
